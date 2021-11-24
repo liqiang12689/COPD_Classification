6988b0cf26b5b5c1de3553ab2c4d4805e8ea822e
NAMZseng <nanrong_z@foxmail.com>
2021-11-25 00:12:06 +0800

    add README.md

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import utils
 
-# dicom_path = "F:\\SZTU\\呼吸专科\\数据\\广医数据\\CTDATA\\E0001001V1\\E0001001V1FC03"
-dicom_path = "F:\\Others\\E0001001V1FC03"
+dicom_path = "/data/zengnanrong/CTDATA/E0001001V1"
 input_image = utils.get_input_image(dicom_path)
 
 print(type(input_image))
